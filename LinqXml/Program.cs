4643d3a0676ec9249ef864c6aec5a6fa02c4f43c
Daniil <pechenkin031199@mail.ru>
2021-12-16 17:56:23 +0500

    Add project files.

---
+++ LinqXml/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LinqXml
+{
+    class Program
+    {
+        //LinqXml11. Дан XML-документ.Найти все различные имена его элементов и вывести каждое 
+        //найденное имя вместе с числом его вхождений в документ.Имена элементов выводить
+        //в порядке их первого вхождения.
+        //Указание.Использовать метод GroupBy.
+
+        static void Main(string[] args)
+        {
+            var doc = XDocument.Load("test.xml");
+            var result = doc.Descendants()
+                            .GroupBy(e => e.Name.LocalName)
+                            .Select(j => new { elemName = j.Key, count = j.Count() });
+            foreach (var elem in result)
+                Console.WriteLine(elem.elemName, elem.count);
+        }
+    }
+}
